e83ac65186488ce0e0e2eb1ee07bd01f2bfd5d23
Marta <martatur1986@gmail.com>
2021-11-14 18:47:51 +0100

    gui

---
+++ cards_operations.py
@@ -0,0 +1,63 @@
+from card import Card
+import random
+import main
+
+
+def Read_file_with_words(number_of_cards):
+    words_list = []
+    chosen_words_list = []
+    with open("dictionary.txt") as file:
+        for line in file:
+            words_list.append(line)
+
+    all_words_count = len(words_list)
+    while len(chosen_words_list) != number_of_cards:
+        index = random.randint(0, all_words_count)
+        chosen_word = words_list[index]
+        if chosen_word not in chosen_words_list and "ść" not in chosen_word:
+            chosen_words_list.append(chosen_word.rstrip('\n'))
+
+    return chosen_words_list
+
+
+def Add_cards(cards_quantity, agent_value, cards_dictionary, available_numbers, words_list):
+    card_number = 1
+    while card_number <= cards_quantity:
+        used_numbers = list(cards_dictionary.keys())
+        id = random.randint(1, available_numbers)
+
+        while id in used_numbers:
+            id = random.randint(1, available_numbers)
+
+        already_existing_words = []
+        for card in cards_dictionary:
+            already_existing_words.append(cards_dictionary[card].word)
+
+        duplicated_word = True
+        while duplicated_word:
+            chosen_word = words_list[random.randint(0, available_numbers - 1)]
+            if chosen_word not in already_existing_words:
+                duplicated_word = False
+
+        cards_dictionary[id] = Card(id, chosen_word, agent_value)
+        card_number += 1
+
+    return cards_dictionary
+
+def Create_all_cards(cards_to_be_created, number_of_cards):
+    all_cards = {}
+    words_list = Read_file_with_words(number_of_cards)
+    for row in cards_to_be_created:
+        all_cards = Add_cards(row[0], row[1], all_cards, number_of_cards, words_list)
+    return all_cards
+
+def Generating_cards(number_of_cards):
+    cards_to_be_created = ((5, "red"), (5, "blue"), (1, "black"), (14, "brown"))
+    all_cards = Create_all_cards(cards_to_be_created, number_of_cards)
+    return all_cards
+#
+#
+# if __name__ == "__main__":
+#     number_of_cards = 25
+#     main(number_of_cards)
+#     gui.Run_gui(all_cards)
--- main.py
+++ main.py
@@ -1,61 +1,87 @@
-from card import Card
-import random
+import pygame
+import cards_operations
+
+pygame.init()
+clock = pygame.time.Clock()
+clock.tick(120)
+window = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption("Agent")
+pink = (99, 92, 107)
+green = (20, 200, 20)
+red = (255, 87, 20)
+black = (10, 10, 10)
+blue = (70, 125, 227)
+brown = (122, 105, 35)
+white = (255, 255, 255)
+window.fill(pink)
+
+class Rect(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height,color,card_instance):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.word = card_instance.word
+        self.card_id = card_instance.card_id
+        self.agent = card_instance.agent
+        self.visible = card_instance.visible
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.font = pygame.font.SysFont("Arial", 25)
+        self.textSurf = self.font.render(self.word, 1, white)
+        W = self.textSurf.get_width()
+        H = self.textSurf.get_height()
+        self.width = width
+        self.height = height
+        self.W = W
+        self.H = H
+        self.update_text()
+        pygame.display.update()
+    def update_text(self):
+        self.image.blit(self.textSurf, [self.width / 2 - self.W / 2, self.height / 2 - self.H / 2])
+
+    def change_value(self):
+        if self.agent == 'red':
+            self.image.fill(red)
+        elif self.agent == 'blue':
+            self.image.fill(blue)
+        elif self.agent == 'black':
+            self.image.fill(black)
+        else:
+            self.image.fill(brown)
+        self.update_text()
+        pygame.display.update()
+
+run = True
+all_cards = cards_operations.Generating_cards(25)
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
 
-def Read_file_with_words(number_of_cards):
-    words_list = []
-    chosen_words_list = []
-    with open("dictionary.txt") as file:
-        for line in file:
-            words_list.append(line)
+    rects = pygame.sprite.Group()
 
-    all_words_count = len(words_list)
-    while len(chosen_words_list) != number_of_cards:
-        index = random.randint(0, all_words_count)
-        chosen_word = words_list[index]
-        if chosen_word not in chosen_words_list and "ść" not in chosen_word:
-            chosen_words_list.append(chosen_word.rstrip('\n'))
+    i = 0
+    x = 250
+    y = 50
+    while i < 25:
+        i += 1
+        card = Rect(x, y, 175, 125, green, all_cards[i])
+        rects.add(card)
+        if i % 5 == 0:
+            y += 150
+            x = 250
+        else:
+            x += 200
 
-    return chosen_words_list
+    if event.type == pygame.MOUSEBUTTONUP:
+        pos = pygame.mouse.get_pos()
+        clicked_sprite = [s for s in rects if s.rect.collidepoint(pos)][0]
+        clicked_sprite.change_value()
+        rects.draw(window)
 
-
-def Add_cards(cards_quantity, agent_value, cards_dictionary, available_numbers, words_list):
-    card_number = 1
-    while card_number <= cards_quantity:
-        used_numbers = list(cards_dictionary.keys())
-        id = random.randint(1, available_numbers)
-
-        while id in used_numbers:
-            id = random.randint(1, available_numbers)
-
-        already_existing_words = []
-        for card in cards_dictionary:
-            already_existing_words.append(cards_dictionary[card].word)
-
-        duplicated_word = True
-        while duplicated_word:
-            chosen_word = words_list[random.randint(0, available_numbers - 1)]
-            if chosen_word not in already_existing_words:
-                duplicated_word = False
-
-        cards_dictionary[id] = Card(id, chosen_word, agent_value)
-        card_number += 1
-
-    return cards_dictionary
-
-def Create_all_cards(cards_to_be_created, number_of_cards):
-    all_cards = {}
-    words_list = Read_file_with_words(number_of_cards)
-    for row in cards_to_be_created:
-        all_cards = Add_cards(row[0], row[1], all_cards, number_of_cards, words_list)
-    return all_cards
-
-def main(number_of_cards):
-    cards_to_be_created = ((5, "red"), (5, "blue"), (1, "black"), (14, "brown"))
-    all_cards = Create_all_cards(cards_to_be_created, number_of_cards)
-    return all_cards
-
+    rects.draw(window)
 
 if __name__ == "__main__":
     number_of_cards = 25
-    main(number_of_cards)
